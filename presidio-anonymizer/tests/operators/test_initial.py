1004f1e97d6b26c34c8d3dd5d4dba22621aa2157
Victor <theowlvic@users.noreply.github.com>
2025-11-18 20:30:14 +0000

    Modified the test to remove trailing spaces and to retain non-alphanumeric values

--- presidio-anonymizer/tests/operators/test_initial.py
+++ presidio-anonymizer/tests/operators/test_initial.py
@@ -5,11 +5,24 @@ from presidio_anonymizer.operators import Initial
 @pytest.mark.parametrize(
     "input_text, initials",
     [
-        ("John Smith", "J. S."),
+        (" John Smith ", "J. S."),
     ],
 )
 def test_given_value_for_initial(input_text, initials):
     text = Initial().operate(input_text) == initials
     assert text
+
 def test_correct_name():
     assert Initial().operator_name() == "initial"
+
+@pytest.mark.parametrize(
+    "input_text, expected",
+    [
+        ("@abc", "@A."), #fulfills alphabetical scope
+        ("@834A", "@8."), #fulfills numerical scope
+        ("--**abc", "--**A.") #fulfills multiple preceeding characters
+    ],
+)
+def test_initial_with_preceeding_special_characters(input_text, expected):
+    result = Initial().operate(input_text)
+    assert result == expected
